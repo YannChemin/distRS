085342dc03ff3418626afc17a2fd72480952a9e2
Yann Chemin <yann.chemin@gmail.com>
2014-07-10 09:28:29 +0530

    updated

--- prog/prog_freq/run.sh
+++ prog/prog_freq/run.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-root=/home/user/Dir/
-files=$(ls $root/*satelliteimages*)
-./freq outFREQ $files  
+#Code can open 15000 files
+#But Linux by default can open 1024 only
+
+#if "Too many open files" error
+#sudo su
+#vim /etc/security/limits.conf
+# # Limit user nofile - max number of open files
+# * soft  nofile 15000
+# * hard  nofile 18000
+#Then restart the user session (i.e., logout, login). 
+
+#Go to Data Directory
+cd ~/DATA
+
+PRoot=~/dev/distRS/trunk
+
+#FREQUENCY PROCESSING
+prog_root=$PRoot/prog/prog_freq
+cd $prog_root
+make clean
+make
+
+cd ~/DATA
+files=$(ls water_*.tif)
+
+outFREQ=~/DATA/water_freq.tif
+
+$prog_root/freq $outFREQ $files  
