ba80acfe5d1f29e784ebfa3463fced0d39ff4e40
Yann Chemin <ychemin@gmail.com>
2018-08-13 10:02:09 +0200

    update

--- prog/prog_biomass_prec/bp.c
+++ prog/prog_biomass_prec/bp.c
@@ -2,25 +2,25 @@
 #include "gdal.h"
 #include<omp.h>
 
-/*
--1	Fill/No Data	Not Processed
-0 	Good Data 	Use with confidence
-1 	Marginal data	Useful, but look at other QA information
-2 	Snow/Ice 	Target covered with snow/ice
-3 	Cloudy 		Target not visible, covered with cloud
-*/
+#define NODATA 28768
+
 void usage()
 {
 	printf( "-----------------------------------------\n");
 	printf( "--Modis Processing chain--Serial code----\n");
 	printf( "-----------------------------------------\n");
-	printf( "./ndvi inNDVI inNDVI_QA\n");
+	printf( "./ndvi inNDVI inET inPET\n");
 	printf( "\toutNDVI\n");
+	printf( "\tDOY Tsw\n");
 	printf( "-----------------------------------------\n");
 	printf( "inNDVI\t\tModis MOD13Q1 NDVI 250m\n");
-	printf( "inNDVI_QA\t\tModis MOD13Q1 NDVI Reliability\n");
+	printf( "inET\t\tModis MOD16A2 ET 500m\n");
+	printf( "inPET\t\tModis MOD16A2 PET 500m\n");
 
 	printf( "outNDVI\tQA corrected NDVI output [-]\n");
+
+	printf( "DOY\tDay of Year\n");
+	printf( "Tsw\tTransmissivity single-way [-]\n");
 	return;
 }
 
@@ -29,47 +29,90 @@ double solar_day(double lat, double doy, double tsw);
 
 int main( int argc, char *argv[] )
 {
-	if( argc < 4 ) {
+	if( argc < 6 ) {
 		usage();
 		return 1;
 	}
-	char	*inB2	 	= argv[1]; //NDVI
-	char	*inB3	 	= argv[2]; //NDVI_QA
-	char	*ndviF	 	= argv[3];
+	char	*inB3	 	= argv[1]; //NDVI_QA 250m
+	char	*inB4	 	= argv[2]; // ET 500m
+	char	*inB5	 	= argv[3]; // PET 500m
+	char	*bpF	 	= argv[4];
+	int	doy		= atoi(argv[5]); // DOY for solar day
+	float	tsw		= atof(argv[6]); // TSW for solar day
+
 	GDALAllRegister();
-	GDALDatasetH hD2 = GDALOpen(inB2,GA_ReadOnly);//NDVI
-	GDALDatasetH hD3 = GDALOpen(inB3,GA_ReadOnly);//NDVI_QA
-	if(hD2==NULL||hD3==NULL){
+	GDALDatasetH hD3 = GDALOpen(inB3,GA_ReadOnly);//NDVI 250m
+	GDALDatasetH hD4 = GDALOpen(inB4,GA_ReadOnly);//ET 500m
+	GDALDatasetH hD5 = GDALOpen(inB5,GA_ReadOnly);//PET 500m
+	if(hD3==NULL||hD4==NULL||hD5==NULL){
 		printf("One or more input files ");
 		printf("could not be loaded\n");
 		exit(1);
 	}
-	GDALDriverH hDr2 = GDALGetDatasetDriver(hD2);
-	GDALDatasetH hDOut = GDALCreateCopy(hDr2,ndviF,hD2,FALSE,NULL,NULL,NULL);
+	//LOAD 250 m bands and create output
+	GDALDriverH hDr3 = GDALGetDatasetDriver(hD3);
+	GDALDatasetH hDOut = GDALCreateCopy(hDr3,bpF,hD3,FALSE,NULL,NULL,NULL);
 	GDALRasterBandH hBOut = GDALGetRasterBand(hDOut,1);
-	GDALRasterBandH hB2 = GDALGetRasterBand(hD2,1);//NDVI
-	GDALRasterBandH hB3 = GDALGetRasterBand(hD3,1);//NDVI_QA
-	int nX = GDALGetRasterBandXSize(hB2);
-	int nY = GDALGetRasterBandYSize(hB2);
-	int N = nX*nY;
-	float *l2 = (float *) malloc(sizeof(float)*N);
-	float *l3 = (float *) malloc(sizeof(float)*N);
-	float *lOut = (float *) malloc(sizeof(float)*N);
-	int rowcol;
-	GDALRasterIO(hB2,GF_Read,0,0,nX,nY,l2,nX,nY,GDT_Float32,0,0);
-	GDALRasterIO(hB3,GF_Read,0,0,nX,nY,l3,nX,nY,GDT_Float32,0,0);
-	#pragma omp parallel for default(none) \
-	private (rowcol) shared (N, l2, l3, lOut)
-	for(rowcol=0;rowcol<N;rowcol++){
-		if( l3[rowcol] == 0||l3[rowcol] == 1||l3[rowcol] == 2||l3[rowcol] == 3) lOut[rowcol] = l2[rowcol];
-		else lOut[rowcol] = -28768;
+	GDALRasterBandH hB3 = GDALGetRasterBand(hD3,1);//NDVI 250m
+	GDALRasterBandH hB4 = GDALGetRasterBand(hD4,1);//ET 500m
+	GDALRasterBandH hB5 = GDALGetRasterBand(hD5,1);//PET 500m
+	double 	geomx[6]={0.0};
+	if(GDALGetGeoTransform(hD3,geomx)==CE_None){
+	/* Do Nothing */
+	//printf( "Origin (ULx,ULy) = (%.6f,%.6f)\n", geomx[0], geomx[3] );
+	//printf( "Pixel Size = (%.6f,%.6f)\n", geomx[1], geomx[5] );
+	//printf( "Rot0 = (%.6f,%.6f)\n", geomx[2], geomx[4] );
+	} else {
+		printf("ERROR: Projection acquisition problem from Band1\n");
+		exit(1);
 	}
-	#pragma omp barrier
-	GDALRasterIO(hBOut,GF_Write,0,0,nX,nY,lOut,nX,nY,GDT_Float32,0,0);
-	if( l2 != NULL ) free( l2 );
-	if( l3 != NULL ) free( l3 );
-	GDALClose(hD2);
+	int nX = GDALGetRasterBandXSize(hB3);//From NDVI@250m
+	int nY = GDALGetRasterBandYSize(hB3);//From NDVI@250m
+	int16_t *l3 = (int16_t *) malloc(sizeof(int16_t)*nX);
+	int16_t *l4 = (int16_t *) malloc(sizeof(int16_t)*nX/2);
+	int16_t *l5 = (int16_t *) malloc(sizeof(int16_t)*nX/2);
+	unsigned int *lOut = (unsigned int *) malloc(sizeof(unsigned int) *nX);
+	int row,col;
+	int minimum=100000.0;
+	int maximum = 0.0;
+	for(row=0;row<nY;row++){
+		GDALRasterIO(hB3,GF_Read,0,row,nX,1,l3,nX,1,GDT_Int16,0,0);
+		GDALRasterIO(hB4,GF_Read,0,row/2,nX/2,1,l4,nX/2,1,GDT_Int16,0,0);
+		GDALRasterIO(hB5,GF_Read,0,row/2,nX/2,1,l5,nX/2,1,GDT_Int16,0,0);
+		#pragma omp parallel for default(none) \
+			private (col) \
+			shared (row,geomx,doy,tsw,nX,nY,l3,l4,l5,lOut,minimum,maximum)
+		for(col=0;col<nX;col++){
+			if(l4[col/2]<32000||l5[col/2]<32000){ 
+				double lat=geomx[3]+geomx[4]*col+geomx[5]*row;
+				double solar = solar_day(lat, doy, tsw );
+				if(solar<0.0) solar=0.0;
+				double evapfr = l4[col/2]/(1.0*l5[col/2]); 
+				if(evapfr<0.0) evapfr=0.0;
+				double fpar=0.0;
+				fpar = 1.257*(l3[col]/10000.0)-0.161;
+				if(fpar<0.0) fpar=0.0;
+				lOut[col]=(unsigned int) 10000.0*biomass(fpar,solar,evapfr,1.0);
+				if(lOut[col]<minimum)minimum=lOut[col];
+				if(lOut[col]>maximum)maximum=lOut[col];
+			}else{
+				lOut[col] = NODATA;
+			}
+		}
+		#pragma omp barrier
+		GDALRasterIO(hBOut,GF_Write,0,row,nX,1,lOut,nX,1,GDT_UInt32,0,0);
+	}
+	printf("min/max: %f %f [kg/ha/day]\n",minimum/10000.0,maximum/10000.0);
+	GDALSetRasterNoDataValue(hBOut,NODATA);
+	GDALSetRasterUnitType(hBOut,"Biomass [kg/ha/day]");
+	GDALSetDescription(hBOut,"250m 8 Days mean Biomass");
+	GDALSetMetadataItem(hDOut,"*","",NULL);
+	if(l3 != NULL) free(l3);
+	if(l4 != NULL) free(l4);
+	if(l5 != NULL) free(l5);
 	GDALClose(hD3);
+	GDALClose(hD4);
+	GDALClose(hD5);
 	GDALClose(hDOut);
 	return(EXIT_SUCCESS);
 }
--- prog/prog_biomass_prec/run.sh
+++ prog/prog_biomass_prec/run.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
 DataRoot=~/RSDATA/ETb/
 root=$DataRoot/3_Products/
-in_ndvi=$DataRoot/2_PreProcessed/NDVI/MDB/
-in_ndvi_qa=$DataRoot/2_PreProcessed/NDVI_QA/MDB/
-out_ndvi=$root/NDVI/
-mkdir $out_ndvi
-rm $out_ndvi/*.tif -f
 
-pre=MOD13A2_
+preN=mod13q1_
+preE=mod16a2_
+post=modq1_BP_
+ndvi=VI_
+et=ETQC_
+pet=PETQC_
+DataRoot=$(pwd)
+in_ndvi=$root/NDVI/
+in_et=$root/ET/
+in_pet=$root/PET/
+out_bp=$root/BP/
 
-for (( doy = 2010000 ; doy <= 2011000 ; doy++ ))
-do test1=$(find $in_ndvi -type f | grep $pre$doy | wc -l)
-  test2=$(find $in_ndvi_qa -type f | grep $pre$doy | wc -l)
-  test3=$(find $out_ndvi -type f | grep $out_ndvi$pre$doy\_1Km_16D_NDVI.tif | wc -l)
-  if [ $test1 -eq 1 -a $test2 -eq 1 -a $test3 -eq 0 ]
-  then inB2=$in_ndvi$pre$doy\_1Km_16D_NDVI.tif
-   inB3=$in_ndvi_qa$pre$doy\_1Km_16D_NDVI_QA.tif
-   outNDVI=$out_ndvi$pre$doy\_1Km_16D_NDVI.tif
-   echo "./ndvi $inB2 $inB3 $outNDVI"
-   ./ndvi $inB2 $inB3 $outNDVI
+mkdir -p $out_bp
+
+#for (( doy = 2001000 ; doy <= 2018000 ; doy++ ))
+for (( doy = 2001000 ; doy <= 2016000 ; doy++ ))
+do
+  test1=$(find $in_ndvi -type f | grep $preN$ndvi | grep $doy | wc -l)
+  if [ $test1 -eq 1 ]
+  then
+	A=$in_ndvi$preN$ndvi$doy.tif
+	#A=mod13q1_NDVI_2001049.tif
+  fi
+  test3=$(find $in_et -type f | grep $preE$et | grep $doy | wc -l)
+  test4=$(find $in_pet -type f | grep $preE$pet | grep $doy | wc -l)
+  test5=$(find $out_ndvi -type f | grep $out_bp$post$doy.tif | wc -l)
+  if [ $test3 -eq 1 -a $test4 -eq 1 -a $test5 -eq 0 ]
+  then 	
+   	B=$in_et$preE$et$doy.tif
+   	C=$in_pet$preE$pet$doy.tif
+   	D=$out_bp$post$doy.tif
+	#B=mod16a2_ET_2001049.tif
+	#C=mod16a2_PET_2001049.tif
+	#D=modq1_BP_2001049.tif
+	E=$(echo $A | sed 's/mod13q1_VI_....\(.*\).tif/\1/')
+	F=0.75
+	./bp $A $B $C $D $E $F 
   fi
 done
+
