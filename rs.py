6c404d2deff0a2b33d1c5d7e4f9976c69103fefc
Yann Chemin <yann.chemin@gmail.com>
2020-09-07 11:39:22 +0200

    update RS functions

--- rs.py
+++ rs.py
@@ -109,20 +109,105 @@ def water(ndvi, albedo):
     return ((ndvi < 0.1) and (albedo < 0.1))
 
 def waterModis(ndvi, band7):
-"""
-# water.modis: Terra-MODIS water mapping tool
-# Xiao X., Boles S., Liu J., Zhuang D., Frokling S., Li C., Salas W., Moore III B. (2005).
-# Mapping paddy rice agriculture in southern China using multi-temporal MODIS images.
-# Remote Sensing of Environment 95:480-492.
-#
-# Roy D.P., Jin Y., Lewis P.E., Justice C.O. (2005).
-# Prototyping a global algorithm for systematic fire-affected
-# area mapping using MODIS time series data.
-# Remote Sensing of Environment 97:137-162.
-"""
-if((ndvi < 0.1) and (band7 < 0.04)):
-    return(1)
-else:
-    return(0)
+    """
+    # water.modis: Terra-MODIS water mapping tool
+    # Xiao X., Boles S., Liu J., Zhuang D., Frokling S., Li C., Salas W., Moore III B. (2005).
+    # Mapping paddy rice agriculture in southern China using multi-temporal MODIS images.
+    # Remote Sensing of Environment 95:480-492.
+    #
+    # Roy D.P., Jin Y., Lewis P.E., Justice C.O. (2005).
+    # Prototyping a global algorithm for systematic fire-affected
+    # area mapping using MODIS time series data.
+    # Remote Sensing of Environment 97:137-162.
+    """
+    if((ndvi < 0.1) and (band7 < 0.04)):
+        return(1)
+    else:
+        return(0)
 
-}
+def arvi(red, nir, blue):
+    # ARVI: Atmospheric Resistant Vegetation Index
+    # ARVI is resistant to atmospheric effects (in comparison to the NDVI) and is accomplished by a self correcting process for the atmospheric effect in the red nel, using the difference in the radiance between the blue and the red nels. (Kaufman and Tanre 1996)
+    result=(nir - (2 * red - blue)) / (nir + (2 * red - blue))
+    return (result)
+
+def dvi(red, nir):
+    # DVI: Difference Vegetation Index
+    return(nir - red)
+
+def evi(blue, red, nir):
+    # EVI: Enhanced Vegetation Index
+    # Huete A.R., Liu H.Q., Batchily K., vanLeeuwen W. (1997)
+    # A comparison of vegetation indices global set of TM images for EOS-MODIS
+    # Remote Sensing of Environment, 59:440-451.
+    result=2.5 * ((nir - red) / (nir + (6 * red) - (7.5 * blue) + 1))
+    result[result < -1]=-1
+    result[result > 1]=1
+    return (result)
+
+def gari(red, nir, blue, green):
+    # GARI: green atmospherically resistant vegetation index
+    result=(nir - (green - (blue - red))) / (nir - (green - (blue - red)))
+    return (result)
+
+def gemi(red, nir):
+    # GEMI: Global Environmental Monitoring Index
+    result=(((2 * ((nir * nir) - (red * red)) + 1.5 * nir + 0.5 * red) / (nir + red + 0.5)) * (
+            1 - 0.25 * (2 * ((nir * nir) - (red * red)) + 1.5 * nir + 0.5 * red) / (nir + red + 0.5))) - (
+            (red - 0.125) / (1 - red))
+    return (result)
+
+def gvi(blue, green, red, nir, ch5, ch7):
+    # Green Vegetation Index
+    return((-0.2848 * blue - 0.2435 * green - 0.5436 * red + 0.7243 * nir + 0.0840 * ch5 - 0.1800 * ch7))
+
+def ipvi(red, nir):
+    # IPVI: Infrared Percentage Vegetation Index
+    return((nir) / (nir + red))
+
+def msavi2(red, nir):
+    # MSAVI2: second Modified Soil Adjusted Vegetation Index
+    return ((1 / 2) * (2 * (nir + 1) - sqrt((2 * nir + 1) * (2 * nir + 1)) - (8 * (nir - red))))
+
+def msavi(red, nir):
+    # MSAVI: Modified Soil Adjusted Vegetation Index
+    # MSAVI = s(NIR-s*red-a) / (a*NIR+red-a*s+X*(1+s*s))
+    # where a is the soil line intercept, s is the soil line slope, and X  is an adjustment factor which is set to minimize soil noise (0.08 in original papers).
+    return ((1 / 2) * (2 * (nir + 1) - sqrt((2 * nir + 1) * (2 * nir + 1)) - (8 * (nir - red))))
+
+def ndvi(red, nir):
+    # NDVI: Normalized Difference Vegetation Index
+    result=(nir - red) / (nir + red)
+    result[result < -1]=-1
+    result[result > 1]=1
+    return (result)
+
+def pvi(red, nir):
+    # PVI: Perpendicular Vegetation Index
+    # PVI = sin(a)NIR-cos(a)red
+    # for a  isovegetation lines (lines of equal vegetation) would all be parallel to the soil line therefore a=1
+    return ((sin(1) * nir) / (cos(1) * red))
+
+def savi(red, nir):
+    # SAVI: Soil Adjusted Vegetation Index
+    return (((1 + 0.5) * (nir - red)) / (nir + red + 0.5))
+
+def sr(red, nir):
+    # SR: Simple Vegetation ratio
+    return (nir / red)
+
+def vari(blue, green, red):
+    """
+    # VARI: Visible Atmospherically Resistant Index
+    # VARI is the Visible Atmospherically Resistant Index, it was
+    # designed to introduce an atmospheric self-correction
+    # Gitelson A.A., Kaufman Y.J., Stark R., Rundquist D., 2002.
+    # Novel algorithms for estimation of vegetation fraction
+    # Remote Sensing of Environment (80), pp76-87.
+    """
+    return((green - red) / (green + red - blue))
+
+def wdvi(red, nir, slslope=1):
+    # WDVI: Weighted Difference Vegetation Index
+    # slslope: slope of soil line
+    return(nir - slslope * red)
